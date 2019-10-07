1b26e2182aa25645b1af1b1f901dab530d2d1d6e
Westongb <westongb@outlook.com>
2019-10-07 12:33:13 -0600

    "First commit"
    "First-Commit"
    git commit -m "First Commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  fetch(url)
+      .then (function (response) {
+        console.log(response);
+        return response.json()
+       })
+        .then(function(data){
+          console.log(data.bpi.GBP.rate)
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Marvel API</h1>
     </div>
   );
 }
